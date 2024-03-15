61b12dbd3951d7bc27c64f98adfd3e090be475d8
mpomirski <michal.pomirski01@gmail.com>
2024-03-15 10:55:54 +0100

    Wersja uproszczona done

--- Wersja uproszczona/main.py
+++ Wersja uproszczona/main.py
@@ -2,7 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import multiprocessing as mp
-
+import os
+import time
 # Random walk matrix
 probability_matrix = np.matrix([
     [1,     0,      0,      0,      0,      0],
@@ -20,13 +21,20 @@ print('The result the random walk starting from p3 will end in p0:')
 print(f'{result[2]:.2f}')
 
 def random_walk(n):
+    np.random.seed((os.getpid() * int(time.time())) % 123456789)
     position = 3
+    successes = 0
     for _ in range(n):
-        position += np.random.choice([-1, 1], p=[0.5, 0.5])
-    return position
+        while position not in [0, 5]:
+            position += np.random.choice([-1, 1], p=[0.5, 0.5])
+        if position == 0:
+            successes += 1
+        position = 3
+    return successes
 
-# Monte Carlo simulation
+#TODO: Monte Carlo simulation
 successes = 0
+
 trials = 100000
 batches = 10
 batch_size = trials // batches
@@ -34,7 +42,7 @@ pool = mp.Pool(mp.cpu_count())
 results = [pool.apply_async(random_walk, args=(batch_size,)) for _ in range(batches)]
 pool.close()
 pool.join()
-successes = np.sum(results == 0)
+successes = sum(result.get() for result in results)
 print('The result of the Monte Carlo simulation:')
 print(f'{successes / trials:.2f}')
 
