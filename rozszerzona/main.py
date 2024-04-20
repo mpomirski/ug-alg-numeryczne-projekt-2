95be3a0887d2305b2eabc1fe64402120aa30d26e
Konrad Kreczko <konradkreczko@gmail.com>
2024-04-20 23:55:51 +0200

    Revert "kinda works but not yet"
    This reverts commit a0916dbca2d18d617f968f6a71b051b1040e3bd0.

--- rozszerzona/main.py
+++ rozszerzona/main.py
@@ -1,106 +1,109 @@
 import numpy as np
-import random
-import networkx as nx
-import matplotlib.pyplot as plt
 
 
-def build_park(input_matrix):
-    G = nx.Graph()
-    id_map = {}
-    node_count = 0
-    for intersection1, intersection2, steps in input_matrix:
-        if intersection1 not in id_map:
-            id_map[intersection1] = node_count
-            node_count += 1
-        if intersection2 not in id_map:
-            id_map[intersection2] = node_count
-            node_count += 1
-
-        prev_node = id_map[intersection1]
-        for step in range(1, steps + 1):
-            step_node = node_count
-            G.add_node(step_node)
-            G.add_edge(prev_node, step_node)
-            prev_node = step_node
-            node_count += 1
-        G.add_edge(prev_node, id_map[intersection2])
-
-    return G, id_map
+# , alleys, exits, sewage_manhole, starting_points
+def build_matrix(n, m):
+    matrix_size = n * m
+    zero_vector = np.ones(matrix_size)
+    matrix = np.diag(zero_vector)
+    return matrix
 
 
-def adjacency_matrix(G):
-    nodes = sorted(G.nodes())
-    n = len(nodes)
-    adj_matrix = np.zeros((n, n), dtype=int)
-    for i, node in enumerate(nodes):
-        for neighbor in G.neighbors(node):
-            adj_matrix[i][nodes.index(neighbor)] = 1
-    return adj_matrix, nodes
+def solve_gauss(matrix, b):
+    """
+    Function to solve the system of equations using Gaussian elimination without pivoting.
 
+    Args:
+        matrix (numpy.ndarray): The system of equations matrix.
+        b (numpy.ndarray): The free term vector.
 
-def generate_transition_matrix(n, adjacency_list):
-    transition_matrix = np.zeros((n, n))
+    Returns:
+        numpy.ndarray: The solution vector or None if the matrix is singular.
+    """
+
+    n = len(b)
+
     for i in range(n):
-        neighbors = np.where(adjacency_list[i] == 1)[0]
-        num_neighbors = len(neighbors)
-        for neighbor in neighbors:
-            transition_matrix[i][neighbor] = 1 / num_neighbors
-    return transition_matrix
+        if matrix[i][i] == 0:
+            return None
+
+        for j in range(i + 1, n):
+            ratio = matrix[j][i] / matrix[i][i]
+            for k in range(n):
+                matrix[j][k] -= ratio * matrix[i][k]
+                b[j] -= ratio * b[i]
+
+    x = np.zeros(n)
+    for i in range(n - 1, -1, -1):
+        x[i] = b[i] / matrix[i][i]
+        for j in range(i - 1, -1, -1):
+            b[j] -= matrix[j][i] * x[i]
+
+    return x
 
 
-def generate_equations(transition_matrix, osk, wyjscie, n):
-    A = np.eye(n)
-    b = np.zeros(n)
-    for i in range(n):
-        if i == osk:
-            A[i][i] = 1
-            b[i] = 0
-        elif i == wyjscie:
-            A[i][i] = 1
-            b[i] = 1
-        else:
-            A[i][i] = 1
-            A[i] -= transition_matrix[i]
-    return A, b
+def monte_carlo_simulation(matrix, b, num_trials):
+    """
+    Function to perform Monte Carlo simulation to verify the solution.
 
+    Args:
+        matrix (numpy.ndarray): The system of equations matrix.
+        b (numpy.ndarray): The free term vector.
+        num_trials (int): Number of trials to simulate.
 
-def monte_carlo(graph, start, target, pit, num_iterations=10000):
-    num_target_reached = 0
-    for _ in range(num_iterations):
-        current_node = start
-        while True:
-            neighbors = list(graph.adj[current_node])
-            next_node = random.choice(neighbors)
-            if next_node == target:
-                num_target_reached += 1
-                break
-            elif next_node == pit:
-                break
-            current_node = next_node
-    return num_target_reached / num_iterations
+    Returns:
+        float: The estimated probability of safe return home.
+    """
+
+    num_safe_returns = 0
+    n = len(b)
+
+    for _ in range(num_trials):
+        x = np.random.rand(n)
+        if np.all(np.dot(matrix, x) - b == 0):
+            num_safe_returns += 1
+
+    return num_safe_returns / num_trials
 
 
 def main():
-    input_matrix = [(1, 2, 3), (2, 3, 2)]  # Example input data
-    start = 2  # Start node ID
-    osk = 3  # OSK node ID
-    wyjscie = 1  # Exit node ID
-    num_iterations = 10000  # Number of Monte Carlo simulations
+    # Read input data
+    n, m = map(int, input().split())
+    # alleys = [list(map(int, input().split())) for _ in range(m)]
+    # sewage_manhole = int(input())
+    # exits = list(map(int, input().split()))
+    # starting_points = list(map(int, input().split()))
+    # matrix, b = build_matrix(n, m, alleys, exits, sewage_manhole, starting_points)
+    # resolve = solve_gauss(matrix, b)
+    # matrix, b = build_matrix(n, m, alleys, exits, sewage_manhole, starting_points)
+    # 3 2
+    # 1 2 3
+    # 2 3 2
+    # 3
+    # 1
+    # 2
+    # should be 0.4
+    matrix = build_matrix(n, m)
+    print("System of Equations Matrix:")
+    for row in matrix:
+        print("  ", row)
 
-    G, id_map = build_park(input_matrix)
-    adj_matrix, nodes = adjacency_matrix(G)
-    transition_matrix = generate_transition_matrix(len(nodes), adj_matrix)
-    A, b = generate_equations(
-        transition_matrix, id_map[osk], id_map[wyjscie], len(nodes)
-    )
-    probabilities = np.linalg.solve(A, b)
-    mc_probability = monte_carlo(
-        G, id_map[start], id_map[wyjscie], id_map[osk], num_iterations
-    )
-    print(A)
-    print("Probabilities by Gaussian Elimination:", probabilities)
-    print("Monte Carlo Probability:", mc_probability)
+    # print("\nFree Term Vector:")
+    # print("  ", b)
 
+
+#
+# resolve = solve_gauss(matrix, b)
+#
+# print("\nSolution Vector:")
+# if resolve is not None:
+# print("  ", resolve)
+# else:
+# print("  Matrix is singular.")
+#
+# print("\nMonte Carlo Simulation Result:")
+# print("  ", monte_carlo_simulation(matrix, b, 10000))
+#
 
 if __name__ == "__main__":
     main()
