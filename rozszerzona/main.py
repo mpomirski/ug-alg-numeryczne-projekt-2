b189145cfb52ddf0355733e89c252d12b093d693
Konrad Kreczko <konradkreczko@gmail.com>
2024-04-21 23:22:07 +0200

    :skull:

--- rozszerzona/main.py
+++ rozszerzona/main.py
@@ -2,14 +2,12 @@ import numpy as np
 
 
 def build_matrix(n, alleys, exits, sewage_manholes):
-    # Expanded graph to include all nodes and intermediates
     size = n + sum(l - 1 for _, _, l in alleys)
     matrix = np.zeros((size, size))
-
+    free_factors = np.zeros(size)
     current_index = n
     node_mapping = list(range(n)) + [None] * (size - n)
 
-    # Create nodes for intermediates and establish correct connections
     for start, end, length in alleys:
         start, end = start - 1, end - 1
         if length == 1:
@@ -21,17 +19,16 @@ def build_matrix(n, alleys, exits, sewage_manholes):
             for i in range(len(path) - 1):
                 matrix[path[i], path[i + 1]] = matrix[path[i + 1], path[i]] = -1
 
-    # Normalize transitions
     for i in range(size):
         row_sum = -matrix[i].sum()
         if row_sum != 0:
             matrix[i] /= row_sum
         matrix[i, i] = 1
 
-    # Absorbing states for exits and manholes
-    for exit in exits:
-        matrix[exit - 1] = np.zeros(size)
-        matrix[exit - 1, exit - 1] = 1
+    for _exit in exits:
+        matrix[_exit - 1] = np.zeros(size)
+        matrix[_exit - 1, _exit - 1] = 1
+        free_factors[_exit - 1] = 1
     for osk in sewage_manholes:
         matrix[osk - 1] = np.zeros(size)
         matrix[osk - 1, osk - 1] = 1
@@ -40,10 +37,9 @@ def build_matrix(n, alleys, exits, sewage_manholes):
 
 
 def solve_matrix(matrix):
-    # Use simple linear algebra solution since matrix is set up correctly
     n = len(matrix)
     b = np.zeros(n)
-    b[[exit - 1 for exit in exits]] = 1  # Free terms for exits
+    b[[_exit - 1 for _exit in exits]] = 1
     return np.linalg.solve(matrix, b)
 
 
@@ -55,7 +51,7 @@ def monte_carlo_simulation(matrix, num_trials, start_node):
         while True:
             probabilities = matrix[current]
             next_node = np.random.choice(n, p=probabilities)
-            if next_node == current:  # Absorbing state reached
+            if next_node == current:
                 if current in [e - 1 for e in exits]:
                     success_count += 1
                 break
@@ -63,11 +59,11 @@ def monte_carlo_simulation(matrix, num_trials, start_node):
     return success_count / num_trials
 
 
-n = 3  # Number of nodes
-alleys = [(1, 2, 3), (2, 3, 2)]  # Alleys with intermediates
-exits = [1]  # Exit nodes
-sewage_manholes = [3]  # Sewage manhole nodes
-start_node = 1 - 1  # Starting node (zero-indexed)
+n = 3
+alleys = [(1, 2, 3), (2, 3, 2)]
+exits = [1]
+sewage_manholes = [3]
+start_node = 1 - 1
 
 matrix = build_matrix(n, alleys, exits, sewage_manholes)
 probabilities = solve_matrix(matrix)
