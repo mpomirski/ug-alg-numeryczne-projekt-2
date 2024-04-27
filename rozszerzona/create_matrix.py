d69d1c617fe1c75990ed9f3be798b9affa48703e
Konrad Kreczko <konradkreczko@gmail.com>
2024-04-27 23:27:06 +0200

    almost works

--- rozszerzona/create_matrix.py
+++ rozszerzona/create_matrix.py
@@ -9,7 +9,7 @@ def read_file(file_name):
         for line in file:
             parts = list(map(int, line.split()))
             parts[-1] -= 1
-            data.append
+            data.append(parts)
     return data
 
 
@@ -20,7 +20,7 @@ def build_graph(edges):
         graph.add_node(end)
         prev_node = start
         for step in range(1, steps + 1):
-            step_node = f"{start}-{end}-{step}"
+            step_node = f"{start}{end}{step}"
             graph.add_node(step_node)
             graph.add_edge(prev_node, step_node)
             prev_node = step_node
@@ -28,17 +28,24 @@ def build_graph(edges):
     return graph
 
 
-def create_adjacency_list(graph: nx.Graph):
-    adj_list = {node: list(sorted(graph.neighbors(node))) for node in graph.nodes()}
+def create_adjacency_list(graph):
+    adj_list = {
+        node: list(sorted(graph.neighbors(node), key=lambda x: str(x)))
+        for node in graph.nodes()
+    }
     return adj_list
 
 
 def create_transition_matrix(adjacency_list):
     n = len(adjacency_list)
     matrix = np.zeros((n, n))
-    for i, neighbours in adjacency_list.items():
+    for i, (node, neighbours) in enumerate(
+        sorted(adjacency_list.items(), key=lambda x: str(x))
+    ):
         for neighbour in neighbours:
-            matrix[i, neighbour] = 1 / len(neighbours)
+            matrix[
+                i, sorted(adjacency_list.keys(), key=lambda x: str(x)).index(neighbour)
+            ] = 1 / len(neighbours)
     return matrix
 
 
@@ -53,8 +60,31 @@ def draw_graph(graph, filename="graph.png"):
     plt.savefig(filename)
 
 
+def prepare_data(equations, manhole, park_exit):
+    n = len(equations)
+    vector = np.zeros(n)
+    vector[park_exit - 1] = 1
+    equations[manhole - 1] = np.zeros(n)
+    equations[park_exit - 1] = np.zeros(n)
+    equations[park_exit - 1, park_exit - 1] = 1
+    return equations, vector
+
+
 def main():
-    pass
+    input_data = read_file("data.txt")
+    graph = build_graph(input_data)
+    draw_graph(graph, "first.png")
+
+    adjacency_list = create_adjacency_list(graph)
+    trans_matrix = create_transition_matrix(adjacency_list)
+    equations = generate_equations(trans_matrix)
+
+    manhole, park_exit = 3, 1
+    matrix, vector = prepare_data(equations, manhole, park_exit)
+
+    print(adjacency_list)
+    print(matrix)
+    print(vector)
 
 
 if __name__ == "__main__":
